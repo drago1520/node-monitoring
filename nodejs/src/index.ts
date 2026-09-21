81a11d00e1b8a760ed88be77394e3df5bba28c9e
drago1520 <141066422+drago1520@users.noreply.github.com>
2026-09-21 12:40:33 +0300

    feat/otel: add console.log logging.
    - automatically attaches to the trace.
    
    feat/otel: add fetch recording. Signoz automatically adds it to "external APIs" tab based on url. amazing.
    
    - Also added to the trace. Now I can pinpoint how much time fetch() took for each trace. OMG.
    
    fix/hono: capture exeptions, because hono catches then -> 500 response + console.error. No real exception goes to signoz

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,15 +1,31 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
+import { trace } from '@opentelemetry/api'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.onError((err, c) => {
+  trace.getActiveSpan()?.recordException(err)
+  if (err instanceof HTTPException) return err.getResponse()
+  console.error(err)
+  return c.text('Internal Server Error', 500)
 })
 
+app.get('/', async (c) => {
+  const user = await (await fetch('https://randomuser.me/api/')).json()
+  console.log(user)
+  return c.text(`Hello ${user.results[0].name.first}`)
+})
+
+app.get('/error', c => {
+  throw new Error('Test otel errors')
+})
+
+//prettier-ignore
 serve({
   fetch: app.fetch,
   port: 3000
-}, (info) => {
+}, info => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
---
+++ nodejs/telemetry.mjs
@@ -0,0 +1,27 @@
+import { register } from 'import-in-the-middle/register-hooks.mjs'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici'
+import { ConsoleInstrumentation } from '@opentelemetry/instrumentation-console'
+import { RuntimeNodeInstrumentation } from '@opentelemetry/instrumentation-runtime-node'
+import { HostMetricsInstrumentation } from '@opentelemetry/instrumentation-host-metrics'
+
+register()
+
+process.env.OTEL_SERVICE_NAME ??= 'hono'
+process.env.OTEL_EXPORTER_OTLP_ENDPOINT ??= 'http://localhost:4318'
+process.env.OTEL_EXPORTER_OTLP_PROTOCOL ??= 'http/protobuf'
+process.env.OTEL_LOGS_EXPORTER ??= 'otlp'
+
+const sdk = new NodeSDK({
+  instrumentations: [
+    new ConsoleInstrumentation(),
+    new HttpInstrumentation(),
+    new UndiciInstrumentation(),
+    new RuntimeNodeInstrumentation(),
+    new HostMetricsInstrumentation({ metricGroups: ['process.cpu', 'process.memory'] }),
+  ],
+})
+
+sdk.start()
+export default sdk
